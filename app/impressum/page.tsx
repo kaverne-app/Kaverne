d2c76ba0af2d3e546212e3b167248fd09e77e033
kaverne-app <kaverne.app@gmail.com>
2026-09-15 00:22:23 +0200

    Update page.tsx

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -13,10 +13,6 @@ export default function ImpressumPage() {
       <p>
         Tim Fischer
         <br />
-        Kirchbergstraße 22
-        <br />
-        76889 Gleiszellen
-        <br />
         Deutschland
         <br />
         kaverne.app@gmail.com
